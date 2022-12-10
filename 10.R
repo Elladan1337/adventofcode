0afa3242e433fa131d5b2eafb91127915f164ef9
Elladan1337 <kosatka.martin@gmail.com>
2022-12-10 11:27:41 +0100

    Solved Part 2, slightly cleaned up.

--- 10.R
+++ 10.R
@@ -1,8 +1,6 @@
 library(tidyverse)
 
 input <- readLines("input10.txt")
-test_input <- readLines("test10.txt")
-
 
 solve <- function(input){
   cycle <- 0
@@ -21,24 +19,44 @@ solve <- function(input){
   }
   return(values_twenty)}
 
-
-test_solve <- function(input){
+solve2 <- function(input){
+  
+  crt_screen <- matrix(".", nrow = 6, ncol = 40)
+  sprite <- 2
+  level <- 1
   cycle <- 0
-  X <- 1
-  values_twenty <- vector()
+  
   for(i in 1:length(input)){
     if(input[i] == "noop"){
       cycle <- cycle + 1
-      if((cycle - 20) %% 40 == 0){values_twenty <- append(values_twenty, (X * cycle))}
+      if(cycle == 41){cycle <- 1
+      level <- level + 1}
+      if(abs(sprite - cycle) <= 1){crt_screen[level, cycle] <- "#"}
     }
     else{cycle <- cycle + 1
-    if((cycle - 20) %% 40 == 0){values_twenty <- append(values_twenty, (X * cycle))}
+    if(cycle == 41){cycle <- 1
+    level <- level + 1}
+    if(abs(sprite - cycle) <= 1){crt_screen[level, cycle] <- "#"}
     cycle <- cycle + 1
-    if((cycle - 20) %% 40 == 0){values_twenty <- append(values_twenty, (X * cycle))}
-    X <- X + as.numeric(str_extract(input[i], "-?\\d+"))
-    }  
-  print(paste("Cycle: ", cycle, " X_Value: ", X))}
-  return(values_twenty)}
+    if(cycle == 41){cycle <- 1
+    level <- level + 1}
+    if(abs(sprite - cycle) <= 1){crt_screen[level, cycle] <- "#"}
+    sprite <- sprite + as.numeric(str_extract(input[i], "-?\\d+"))
+    }
+  }
+  return(crt_screen)}
 
 ## Part 1
 solve(input)
+
+## Part 2
+write_delim(as.data.frame(solve2(input)), "output10.txt", delim = "", col_name = FALSE)
+
+###..#....####.####.####.#.....##..####.
+#..#.#....#.......#.#....#....#..#.#....
+#..#.#....###....#..###..#....#....###..
+###..#....#.....#...#....#....#.##.#....
+#.#..#....#....#....#....#....#..#.#....
+#..#.####.####.####.#....####..###.####.
+
+
